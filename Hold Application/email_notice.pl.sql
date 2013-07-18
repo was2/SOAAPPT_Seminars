4615cc0d3ab0bd3f8696e922008b7541ad4208ef
was2 <drew@was2.net>
2013-07-18 13:47:09 -0500

    Initial emal notificaiton work
    First attempt at email notification with database - stored text and
    templating

--- Hold Application/email_notice.pl.sql
+++ Hold Application/email_notice.pl.sql
@@ -1,14 +1,16 @@
+DECLARE
+
+CURSOR c_students IS
 -- pidms with unattended seminars
 with unattended_seminars as
      ( select distinct sorappt_pidm pidm
          from sorappt
         where sorappt_recr_code in ('001','002','003','004')
-          and sorappt_rslt_code is null ) 
-          
-select unattended_seminars.pidm
+          and sorappt_rslt_code is null )
+     
+select unattended_seminars.pidm pidm
   
   from unattended_seminars,
-  
        -- credit hours rolled to academic history
        (  select shrtckg_pidm pidm, sum(shrtckg.shrtckg_credit_hours) hours
             from shrtckg, shrtckl, unattended_seminars,
@@ -46,8 +48,46 @@ select unattended_seminars.pidm
                                             in ('10', '20', '30', '40' ) )
           group by sfrstcr_pidm
        ) inprog
-        
+
  where rolled.pidm = unattended_seminars.pidm
    and inprog.pidm = unattended_seminars.pidm
+   and rolled.hours + inprog.hours >= 24;
    
-   and rolled.hours + inprog.hours >= 24
+l_notice_template varchar2(4000);
+   
+BEGIN
+  
+  select text
+    into l_notice_template
+    from mgccop.seminar_notices
+   where label = 'notice01';
+  
+  
+  FOR student in c_students LOOP
+    DECLARE
+      l_notice_text varchar2(4000);
+      l_seminar_descs varchar2(4000);
+      cursor c_seminar_descs (p_pidm varchar2) is
+        select stvrecr_desc from stvrecr, sorappt
+         where sorappt_pidm = p_pidm
+           and sorappt_recr_code in ('001','002','003','004')
+           and sorappt_rslt_code is null
+           and stvrecr_code = sorappt_recr_code;
+    BEGIN
+      FOR descrip in c_seminar_descs(student.pidm) LOOP
+        l_seminar_descs := l_seminar_descs || descrip.stvrecr_desc || utl_tcp.crlf;
+      END LOOP;
+          
+      l_notice_text := replace(l_notice_template, '<<name>>', person_info.f_get_fname(student.pidm));
+      l_notice_text := replace(l_notice_text, '<<seminars>>', l_seminar_descs);
+  
+      mgccop.send_mail( p_to => 'drew.sawyer@mgccc.edu', --student_info.f_get_email( student.pidm )
+                        p_from => 'some.guy@mgccc.edu',
+                        p_subject => 'You have not attended required seminars!',
+                        p_text_msg => l_notice_text,
+                        p_smtp_host => 'mgcccmail.mgccc.edu' );
+    END;
+  END LOOP;
+
+END;
+   
